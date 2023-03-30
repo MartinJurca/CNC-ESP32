f8ce660dcaecaee238bbb65fbee67bc67fab1d73
Martin Jurča <martin.jurcik@post.cz>
2023-03-30 17:02:29 +0200

    světlo na konci tunelu

--- src/main.cpp
+++ src/main.cpp
@@ -15,13 +15,15 @@
 #include "Timer.cpp"
 
 void _FeedTask(void* pvParameters);
+void _XHomingTask(void* pvParameters);
+void _YHomingTask(void* pvParameters);
+void _ZHomingTask(void* pvParameters);
 
 const driverpins mz = {1, 2, 3, 4, 5, 6, 7, 8};
 const driverpins mx = {9, 10, 11, 12, 13, 14, 15, 16};
 const driverpins my = {17, 18, 19, 20, 21, 22, 23, 24};
 STEPPERMOTOR SMX(mx), SMY(my), SMZ(mz);
 FAN MBFAN(12, 0);
-bool enabled = true;
 bool accenabled = false;
 bool feedread = false;
 bool hold = false;
@@ -40,12 +42,421 @@ void setup()
   SPINDLE::psmx = &SMX;
   SPINDLE::psmy = &SMY;
   SPINDLE::psmz = &SMZ;
+  SrBegin();
+  SMX.Begin();
+  SMY.Begin();
+  SMZ.Begin();
+  SMX.SetStepping(1);
+  SMY.SetStepping(1);
+  SMZ.SetStepping(1);
+  AXES::acceleration = 3;
+  AXES::acctreshold = 1000;
+  AXES::xymaxspeed = 100.0;
   xTaskCreatePinnedToCore(_FeedTask, "Feed_task", 4096, nullptr, 1, nullptr, 0);
 }
 
 void loop()
 {
-  //
+  if ((feeder.size() != 0) && (feeder.size() == parameter.size()))
+  {
+    switch (feeder[0])
+    {
+      case 10: //získat stav
+      {
+        Serial.print("[");
+        Serial.print("Xenabled:" + String(SMX.IsEnabled()));
+        Serial.println("]");
+        Serial.print("[");
+        Serial.print("Yenabled:" + String(SMY.IsEnabled()));
+        Serial.println("]");
+        Serial.print("[");
+        Serial.print("Zenabled:" + String(SMZ.IsEnabled()));
+        Serial.println("]");
+        Serial.print("[");
+        Serial.print("Xreal:" + String(AXES::realpoz[0]));
+        Serial.println("]");
+        Serial.print("[");
+        Serial.print("Yreal:" + String(AXES::realpoz[1]));
+        Serial.println("]");
+        Serial.print("[");
+        Serial.print("Zreal:" + String(AXES::realpoz[2]));
+        Serial.println("]");
+        Serial.print("[");
+        Serial.print("Xart:" + String(AXES::artpoz[0]));
+        Serial.println("]");
+        Serial.print("[");
+        Serial.print("Yart:" + String(AXES::artpoz[1]));
+        Serial.println("]");
+        Serial.print("[");
+        Serial.print("Zart:" + String(AXES::artpoz[2])); 
+        Serial.println("]");
+        Serial.print("[");
+        Serial.print("Xflag:" + String(ENDSTOP::flagx));
+        Serial.println("]");
+        Serial.print("[");
+        Serial.print("Yflag:" + String(ENDSTOP::flagy));
+        Serial.println("]");
+        Serial.print("[");
+        Serial.print("Zflag:" + String(ENDSTOP::flagz));
+        Serial.println("]");
+        Serial.print("[");
+        Serial.print("EXZflag:" + String(ENDSTOP::exflagz));
+        Serial.println("]");
+        Serial.print("[");
+        Serial.print("XYspeed:" + String(AXES::xyspeed));
+        Serial.println("]");
+        Serial.print("[");
+        Serial.print("Zspeed:" + String(AXES::zspeed));
+        Serial.println("]");
+        Serial.print("[");
+        Serial.print("acceleration:" + String(AXES::acceleration));
+        Serial.println("]");
+        Serial.print("[");
+        Serial.print("treshold:" + String(AXES::acctreshold));
+        Serial.println("]");
+      }
+      break;
+
+      case 11: //zapnout motor
+      {
+        switch (parameter[0])
+        {
+          case 0:
+          SMX.Enable();
+          break;
+
+          case 1:
+          SMY.Enable();
+          break;
+
+          case 2:
+          SMZ.Enable();
+          break;
+
+          default: Serial.println("[invalid_argument]"); break;
+        }
+      }
+      break;
+
+      case 12: //vypnout motor
+      {
+        switch (parameter[0])
+        {
+          case 0:
+          SMX.Disable();
+          break;
+
+          case 1:
+          SMY.Disable();
+          break;
+
+          case 2:
+          SMZ.Disable();
+          break;
+
+          default: Serial.println("[invalid_argument]");
+        }
+      }
+      break;
+
+      case 13: //zapnout endstop
+      {
+        switch (parameter[0])
+        {
+          case 0:
+          ENDSTOP::XEnable();
+          break;
+
+          case 1:
+          ENDSTOP::YEnable();
+          break;
+
+          case 2:
+          ENDSTOP::ZEnable();
+          ENDSTOP::ExZEnable();
+          break;
+
+          default: Serial.println("[invalid_argument]");
+        }
+      }
+      break;
+
+      case 14: //vypnout endstop
+      {
+        switch (parameter[0])
+        {
+          case 0:
+          ENDSTOP::XDisable();
+          break;
+
+          case 1:
+          ENDSTOP::YDisable();
+          break;
+
+          case 2:
+          ENDSTOP::ZDisable();
+          ENDSTOP::ExZDisable();
+          break;
+
+          default: Serial.println("[invalid_argument]");
+        }
+      }
+      break;
+
+      case 15: //zapnout vřeteno
+      {
+        if (!SPINDLE::Start()) Serial.println("[failed]");
+      }
+      break;
+
+      case 16: //vypnout vřeteno
+      {
+        SPINDLE::Stop();
+      }
+      break;
+
+      case 17: //zapnout manuální ovládání -> implementovat
+      {
+        //
+      }
+      break;
+
+      case 18: //vypnout manuální ovládání -> implementovat
+      {
+        //
+      }
+      break;
+
+      case 19: //nastavit ventilátor
+      {
+        MBFAN.Set((uint8_t)parameter[0]);
+      }
+      break;
+
+      case 20: break;
+
+      case 21: //začít přenos dat -> implementovat
+      {
+        //
+      }
+      break;
+
+      case 22: //najít výchozí polohu os
+      {
+        TaskHandle_t x_th, y_th, z_th;
+        TIMER TMRX(10000UL), TMRY(10000UL), TMRZ(30000UL);
+        TMRX.Update();
+        TMRY.Update();
+        TMRZ.Update();
+        switch (parameter[0])
+        {
+          case 0:
+          {
+            SMX.Enable();
+            float spd = AXES::xyspeed;
+            AXES::xyspeed = 30.0;
+            AXES::iscalibrated[0] = false;
+            ENDSTOP::XEnable();
+            xTaskCreatePinnedToCore(_XHomingTask, "X_homing_task", 2048, nullptr, 1, &x_th, 1);
+            while (!AXES::iscalibrated[0])
+            {
+              if (TMRX.Update())
+              {
+                vTaskDelete(x_th);
+                Serial.println("[Xfailed]");
+                break;
+              }
+              else vTaskDelay(10 / portTICK_PERIOD_MS);
+            }
+            AXES::xyspeed = spd;
+            ENDSTOP::XDisable();
+            if (AXES::iscalibrated[0]) SMX.Enable();
+            if (AXES::iscalibrated[0]) Serial.println("[Xsucces]");
+          }
+          break;
+
+          case 1:
+          {
+            SMY.Enable();
+            float spd = AXES::xyspeed;
+            AXES::xyspeed = 30.0;
+            AXES::iscalibrated[1] = false;
+            ENDSTOP::YEnable();
+            xTaskCreatePinnedToCore(_YHomingTask, "Y_homing_task", 2048, nullptr, 1, &y_th, 1);
+            while (!AXES::iscalibrated[1])
+            {
+              if (TMRY.Update())
+              {
+                vTaskDelete(y_th);
+                Serial.println("[Yfailed]");
+                break;
+              }
+              else vTaskDelay(10 / portTICK_PERIOD_MS);
+            }
+            AXES::xyspeed = spd;
+            ENDSTOP::YDisable();
+            if (AXES::iscalibrated[1]) SMY.Enable();
+            if (AXES::iscalibrated[1]) Serial.println("[Ysucces]");
+          }
+          break;
+
+          case 2:
+          {
+            SMZ.Enable();
+            float spd = AXES::zspeed;
+            AXES::zspeed = 5.0;
+            AXES::iscalibrated[2] = false;
+            ENDSTOP::ExZEnable();
+            xTaskCreatePinnedToCore(_ZHomingTask, "Z_homing_task", 2048, nullptr, 1, &z_th, 1);
+            while (!AXES::iscalibrated[2])
+            {
+              if (TMRZ.Update())
+              {
+                vTaskDelete(z_th);
+                Serial.println("[Zfailed]");
+                break;
+              }
+              else vTaskDelay(10 / portTICK_PERIOD_MS);
+            }
+            AXES::zspeed = spd;
+            ENDSTOP::ExZDisable();
+            if (AXES::iscalibrated[2]) SMZ.Enable();
+            if (AXES::iscalibrated[2]) Serial.println("[Zsucces]");
+          }
+          break;
+
+          case 3:
+          {
+            SMX.Enable();
+            SMY.Enable();
+            SMZ.Enable();
+            bool fx = false, fy = false, fz = false;
+            float xyspd = AXES::xyspeed, zspd = AXES::zspeed;
+            AXES::xyspeed = 30.0, AXES::zspeed = 5.0;
+            AXES::iscalibrated[0] = false;
+            AXES::iscalibrated[1] = false;
+            AXES::iscalibrated[2] = false;
+            ENDSTOP::XEnable();
+            ENDSTOP::YEnable();
+            ENDSTOP::ExZEnable();
+            xTaskCreatePinnedToCore(_XHomingTask, "X_homing_task", 2048, nullptr, 1, &x_th, 1);
+            xTaskCreatePinnedToCore(_YHomingTask, "Y_homing_task", 2048, nullptr, 1, &y_th, 1);
+            xTaskCreatePinnedToCore(_ZHomingTask, "Z_homing_task", 2048, nullptr, 1, &z_th, 1);
+            while ((!AXES::iscalibrated[0] || !AXES::iscalibrated[1]) || !AXES::iscalibrated[2])
+            {
+              if (TMRX.Update())
+              {
+                vTaskDelete(x_th);
+                Serial.println("[Xfailed]");
+                fx = true;
+              }
+              if (TMRY.Update())
+              {
+                vTaskDelete(y_th);
+                Serial.println("[Yfailed]");
+                fy = true;
+              }
+              if (TMRZ.Update())
+              {
+                vTaskDelete(z_th);
+                Serial.println("[Zfailed]");
+                fz = true;
+              }
+              if ((fx && fy) && fz) break;
+              vTaskDelay(10 / portTICK_PERIOD_MS);
+            }
+            AXES::xyspeed = xyspd, AXES::zspeed = zspd;
+            ENDSTOP::XDisable();
+            ENDSTOP::YDisable();
+            ENDSTOP::ExZDisable();
+            if (AXES::iscalibrated[0]) SMX.Enable();
+            if (AXES::iscalibrated[1]) SMY.Enable();
+            if (AXES::iscalibrated[2]) SMZ.Enable();
+            if (!fx) Serial.println("[Xsucces]");
+            if (!fy) Serial.println("[Ysucces]");
+            if (!fz) Serial.println("[Zsucces]");
+          }
+          break;
+
+          default: Serial.println("[invalid_argument]");
+        }
+      }
+      break;
+
+      case 23: //nasatavit virtuální polohu X
+      {
+        AXES::artpoz[0] = parameter[0];
+      }
+      break;
+
+      case 24: //nasatavit virtuální polohu Y
+      {
+        AXES::artpoz[1] = parameter[0];
+      }
+      break;
+
+      case 25: //nasatavit virtuální polohu Z
+      {
+        AXES::artpoz[2] = parameter[0];
+      }
+      break;
+
+      case 26: //zapnout akceleraci
+      {
+        accenabled = true;
+      }
+      break;
+
+      case 27: //vypnout akceleraci
+      {
+        accenabled = false;
+      }
+      break;
+
+      case 28: //nastavit akceleraci
+      {
+        if (parameter[0] < 1) Serial.println("[invalid_argument]");
+        else AXES::acceleration = parameter[0];
+      }
+      break;
+
+      case 29: //nastavit práh akcelerace
+      {
+        if (parameter[0] < 1) Serial.println("[invalid_argument]");
+        else AXES::acctreshold = parameter[0];
+      }
+      break;
+
+      case 30: //nastavit rychlost XY
+      {
+        if (parameter[0] < 1) Serial.println("[invalid_argument]");
+        else
+        {
+          float hld = (float)parameter[0];
+          hld /= 10.0;
+          if (hld > AXES::xymaxspeed) hld = AXES::xymaxspeed;
+          AXES::xyspeed = hld;
+        }
+      }
+      break;
+
+      case 31: //nastavit rychlost Z
+      {
+        if (parameter[0] < 1) Serial.println("[invalid_argument]");
+        else
+        {
+          float hld = (float)parameter[0];
+          hld /= 10.0;
+          if (hld > AXES::zmaxspeed) hld = AXES::zmaxspeed;
+          AXES::zspeed = hld;
+        }
+      }
+      break;
+    }
+    if (feeder.size() != 0) feeder.shift();
+    if (parameter.size() != 0) parameter.shift();
+  }
+  else vTaskDelay(1 / portTICK_PERIOD_MS);
 }
 
 void _FeedTask(void* pvParameters)
@@ -55,6 +466,9 @@ void _FeedTask(void* pvParameters)
   TIMER TMRUPDATE(1000);
   FEED.PridejPrikaz(1, "!", false);
   FEED.PridejPrikaz(2, "test", true);
+  FEED.PridejPrikaz(3, "restart", false);
+  FEED.PridejPrikaz(4, "?", false);
+  FEED.PridejPrikaz(5, "~", false);
   //
   FEED.PridejPrikaz(10, "a", true);
   FEED.PridejPrikaz(11, "b", true);
@@ -92,7 +506,6 @@ void _FeedTask(void* pvParameters)
         {
           case 1:
           {
-            enabled = false;
             SMX.Disable();
             SMY.Disable();
             SMZ.Disable();
@@ -109,6 +522,17 @@ void _FeedTask(void* pvParameters)
           Serial.println("parametr: " + String(param));
           break;
 
+          case 3: ESP.restart();
+
+          case 4:
+          Serial.println("CNC-ESP32");
+          break;
+
+          case 5:
+          feeder.clear();
+          parameter.clear();
+          break;
+
           default:
           {
             feeder.add(tasknum);
@@ -120,7 +544,61 @@ void _FeedTask(void* pvParameters)
       else vTaskDelay(10 / portTICK_RATE_MS);
     }
   }
-  enabled = false;
+  vTaskDelete(nullptr);
+}
+
+void _XHomingTask(void* pvParameters)
+{
+  motion xtohome;
+  xtohome.size = 1;
+  xtohome.psteps = new uint8_t[xtohome.size];
+  xtohome.psteps[0] = 0b10100000;
+  while (true)
+  {
+    if (ENDSTOP::flagx || digitalRead(ENDSTOP::pinx)) break;
+    AXES::ExeMotion(xtohome, false);
+  }
+  AXES::iscalibrated[0] = true;
+  AXES::realpoz[0] = 0;
+  ENDSTOP::flagx = false;
+  xtohome.Clear();
+  vTaskDelete(nullptr);
+}
+
+void _YHomingTask(void* pvParameters)
+{
+  pinMode(ENDSTOP::piny, INPUT);
+  motion ytohome;
+  ytohome.size = 1;
+  ytohome.psteps = new uint8_t[ytohome.size];
+  ytohome.psteps[0] = 0b10000000;
+  while (true)
+  {
+    if (ENDSTOP::flagy || digitalRead(ENDSTOP::piny)) break;
+    AXES::ExeMotion(ytohome, false);
+  }
+  AXES::iscalibrated[1] = true;
+  AXES::realpoz[1] = 0;
+  ENDSTOP::flagy = false;
+  ytohome.Clear();
+  vTaskDelete(nullptr);
+}
+
+void _ZHomingTask(void* pvParameters)
+{
+  pinMode(ENDSTOP::expinz, INPUT_PULLUP);
+  motion ztohome;
+  ztohome.size = 1;
+  ztohome.psteps = new uint8_t[ztohome.size];
+  ztohome.psteps[0] = 0b11000000;
+  while (true)
+  {
+    if (ENDSTOP::exflagz || !digitalRead(ENDSTOP::expinz)) break;
+    AXES::ExeMotion(ztohome, false);
+  }
+  AXES::iscalibrated[2] = true;
+  ENDSTOP::exflagz = false;
+  ztohome.Clear();
   vTaskDelete(nullptr);
 }
 
